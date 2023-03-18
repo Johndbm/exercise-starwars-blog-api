c1adeb4ee7414ff401a2775ea5c21c80de17759d
john.banks <johndbanksm@gmail.com>
2023-03-18 03:42:31 +0000

    added users GET endpoint

--- src/api/routes.py
+++ src/api/routes.py
@@ -3,6 +3,9 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
+from api.models import db, Favorite
+from api.models import db, Person
+from api.models import db, Planet
 from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
@@ -16,3 +19,16 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/users', methods=['GET'])
+def get_users():
+
+    if request.method == "GET":
+        all_users = User.query.all()
+        user_dictionary = []
+        for user in all_users:
+            user_dictionary.append(user.serialize())
+        print(user_dictionary)
+
+    return jsonify(user_dictionary), 200
+
